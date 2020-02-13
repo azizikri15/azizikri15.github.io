e08f1ab1b27cea432b58d3d2359d71d053bfca84
azizikri15 <59533832+azizikri15@users.noreply.github.com>
2020-02-13 11:22:44 +0700

    pre firebase

--- js/googlesignin.js
+++ js/googlesignin.js
@@ -9,12 +9,30 @@
 
 function onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
+    window.location.replace("http://zikriapi.000webhostapp.com/review.html");
+    $("#pic").attr('src', profile.getImageUrl());
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#id").text(profile.getId());
+    $("#user").show();
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function onSignInReview(googleUser) {
+    const profile = googleUser.getBasicProfile();
     // window.location.replace("http://zikriapi.000webhostapp.com/review.html");
     $("#pic").attr('src', profile.getImageUrl());
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#id").text(profile.getId());
     $("#user").show();
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
